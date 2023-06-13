33b87dbd512dd2bbdc2811c62341c186bd99e63b
LeonCarceglia <51706636+LeonCarceglia@users.noreply.github.com>
2023-06-13 20:25:32 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
+import viewsRouter from "./routes/views.router.js";
+import {Server} from "socket.io"
+
+const app = express();
+const httpServer = app.listen(8080, () => console.log("Listening to port 8080"))
+
+const io = new Server(httpServer)
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+app.use("/", viewsRouter);
+
+
+const messages = []
+io.on("connection", socket => {
+  console.log("Nuevo cliente conectado")
+  io.listenerCount("messagesLogs", messages)
+
+  socket.on("message", data =>{
+    messages.push(data)
+    io.emit("messageLogs", messages)
+  })
+})
